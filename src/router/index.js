dcb93df83a198468b104e17dbac99fe37bb41efb
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-10-23 10:19:18 +0700

    New components and views

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../views/HomePage.vue"; // Updated import
-import HabitsPage from "../views/HabitsPage.vue"; // Updated import
-import RewardsPage from "../views/RewardsPage.vue"; // Updated import
+import HomePage from "../views/HomePage.vue";
+import HabitsPage from "../views/HabitsPage.vue";
+import RewardsPage from "../views/RewardsPage.vue";
+import SpinnerPage from "../views/SpinnerPage.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: HomePage, // Updated reference
+    component: HomePage,
   },
   {
     path: "/habits",
     name: "Habits",
-    component: HabitsPage, // Updated reference
+    component: HabitsPage,
   },
   {
     path: "/rewards",
     name: "Rewards",
-    component: RewardsPage, // Updated reference
+    component: RewardsPage,
+  },
+  {
+    path: "/spinner",
+    name: "Spinner",
+    component: SpinnerPage,
   },
 ];
 
---
+++ src/views/SpinnerPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Spinner Page</h1>
+    <p>Spin to win here!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RewardsPage",
+};
+</script>
+
+<style scoped>
+/* Add styles for the rewards page */
+</style>
