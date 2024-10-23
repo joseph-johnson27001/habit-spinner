bb3e8e6a8f8c484bd1eb18a74a7898e03a9ee669
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-10-23 11:04:07 +0700

    Top Bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
   <div id="app">
-    <NavBar />
-
-    <router-view />
-    <!-- This is where the routed components will be rendered -->
+    <TopBar />
+    <div class="content-container">
+      <router-view />
+      <NavBar />
+    </div>
   </div>
 </template>
 
 <script>
+import TopBar from "./components/Navigation/TopBar.vue";
 import NavBar from "./components/Navigation/NavBar.vue";
 
 export default {
   name: "App",
   components: {
+    TopBar,
     NavBar,
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  background-color: #e6f0ff;
+  min-height: 100vh;
+}
+
+.content-container {
+  max-width: 1000px;
+  width: 100%;
+  background-color: white;
+  margin: 0 auto;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  flex-grow: 1;
 }
 </style>
---
+++ src/components/Navigation/TopBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="top-bar">
+    <div class="logo">Habit Spinner</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopBar",
+};
+</script>
+
+<style>
+.top-bar {
+  display: flex;
+  justify-content: center; /* Center the logo */
+  align-items: center; /* Center vertically */
+  background-color: #003366; /* Dark blue background */
+  color: white; /* Text color */
+  padding: 10px 0; /* Padding around the bar */
+  width: 100%; /* Full width */
+}
+
+.logo {
+  font-size: 1.5em; /* Adjust logo font size */
+  font-weight: bold; /* Make logo text bold */
+}
+</style>
