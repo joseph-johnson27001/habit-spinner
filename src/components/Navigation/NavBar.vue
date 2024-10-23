5a8f6d43225aac684bff446cf6be4b2e55587ed0
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-10-23 11:17:03 +0700

    Bottom nav buttons

--- src/components/Navigation/NavBar.vue
+++ src/components/Navigation/NavBar.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="nav-bar">
-    <router-link to="/" class="nav-button">Home</router-link>
-    <router-link to="/habits" class="nav-button">Habits</router-link>
-    <router-link to="/rewards" class="nav-button">Rewards</router-link>
+    <router-link
+      to="/habits"
+      class="nav-button"
+      :class="{ active: $route.path === '/habits' }"
+    >
+      <i class="fas fa-list-alt"></i>
+      <span>Habits</span>
+    </router-link>
+    <router-link
+      to="/"
+      class="nav-button"
+      :class="{ active: $route.path === '/tracker' }"
+    >
+      <i class="fas fa-spinner"></i>
+      <span>Tracker</span>
+    </router-link>
+    <router-link
+      to="/rewards"
+      class="nav-button"
+      :class="{ active: $route.path === '/rewards' }"
+    >
+      <i class="fas fa-gift"></i>
+      <span>Rewards</span>
+    </router-link>
   </div>
 </template>
 
@@ -22,27 +43,32 @@ export default {
   display: flex;
   justify-content: space-around;
   align-items: center;
-  background-color: #003366;
-  color: white;
+  background-color: #f3f2f2;
+  color: #333333;
   padding: 10px 0;
   width: 100%;
-  position: relative;
-  margin-top: auto;
+  position: fixed;
+  bottom: 0;
+  left: 0;
 }
 
 .nav-button {
-  color: white;
+  color: #666666;
   text-decoration: none;
-  padding: 10px 15px;
-  border: none;
-  background: transparent;
-  cursor: pointer;
-  font-weight: 500;
-  border-radius: 5px;
-  transition: background-color 0.3s;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.nav-button:hover {
-  background-color: rgba(255, 255, 255, 0.2);
+.nav-button.active {
+  color: #333333;
+}
+
+.nav-button i {
+  font-size: 20px;
+}
+
+.nav-button span {
+  font-size: 12px;
 }
 </style>
