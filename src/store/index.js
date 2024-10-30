9b6989783334052e98285f48d3a8315688d3cf15
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-10-30 12:30:29 +0700

    Local storage plugin

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,25 @@
 import { createStore } from "vuex";
 import habits from "./modules/Habits";
 
+// LocalStorage plugin
+const localStoragePlugin = (store) => {
+  // Load state from localStorage when the store is initialized
+  const storedState = localStorage.getItem("vuexState");
+  if (storedState) {
+    store.replaceState(JSON.parse(storedState));
+  }
+
+  // Subscribe to mutations to save the state to localStorage
+  store.subscribe((mutation, state) => {
+    localStorage.setItem("vuexState", JSON.stringify(state));
+  });
+};
+
 const store = createStore({
   modules: {
     habits,
   },
+  plugins: [localStoragePlugin], // Add the local storage plugin here
 });
 
 export default store;
