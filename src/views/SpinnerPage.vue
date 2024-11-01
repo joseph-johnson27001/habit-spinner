d6a4501cb597dc1493b5388d816ab45e396a50c0
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-11-01 09:58:23 +0700

    Swapped tracker page views with spinner

--- src/views/SpinnerPage.vue
+++ src/views/SpinnerPage.vue
@@ -2,15 +2,29 @@
   <div>
     <h1>Spinner Page</h1>
     <p>Spin to win here!</p>
+
+    <!-- Display today's completed habits and stored habits -->
+    <div>
+      <p>Today's Completed Habits: {{ todayCompletedHabits }}</p>
+      <p>Stored Completed Habits: {{ storedHabits }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  name: "RewardsPage",
+  name: "SpinnerPage",
+  computed: {
+    ...mapGetters("habits", [
+      "todayCompletedHabits",
+      "storedHabits", // Changed to match the updated state
+    ]),
+  },
 };
 </script>
 
 <style scoped>
-/* Add styles for the rewards page */
+/* Add styles for the spinner page */
 </style>
