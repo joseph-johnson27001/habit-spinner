1b2eb39f6df154cbce3e03b56170ad3dc484b154
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-11-06 10:25:03 +0700

    Fixed bug

--- src/components/Toast/ToastBar.vue
+++ src/components/Toast/ToastBar.vue
@@ -42,7 +42,7 @@ export default {
   methods: {
     startTimer() {
       this.timer = setTimeout(() => {
-        this.isVisible = true;
+        this.isVisible = false;
       }, 3000);
     },
     clearTimer() {
