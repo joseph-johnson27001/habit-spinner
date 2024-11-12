234855f0c823658cd5b84267d71a8446b9da7527
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-11-12 07:57:14 +0700

    Levels and points in state

--- src/store/modules/Levels.js
+++ src/store/modules/Levels.js
@@ -2,8 +2,29 @@ export default {
   namespaced: true,
   state: () => ({
     level: 1,
+    points: 0, // Track points for the current level
+    levelRequirements: Array.from({ length: 100 }, (_, i) => (i + 1) * 25), // XP required for each level is 25 points higher than previous level
   }),
   getters: {
     getLevel: (state) => state.level,
+    getPoints: (state) => state.points,
+    getPointsToNextLevel: (state) => state.levelRequirements[state.level - 1], // Points required for the next level
+  },
+  mutations: {
+    ADD_POINTS(state, points) {
+      state.points += points;
+      const pointsToNextLevel = state.levelRequirements[state.level - 1];
+
+      // Check if user has enough points to level up
+      if (state.points >= pointsToNextLevel) {
+        state.level += 1; // Increase level
+        state.points = 0; // Reset points for the new level
+      }
+    },
+  },
+  actions: {
+    addPoints({ commit }, points) {
+      commit("ADD_POINTS", points);
+    },
   },
 };
