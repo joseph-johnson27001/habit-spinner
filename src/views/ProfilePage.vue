21b265f7504b0f1f8df23d066d9bfc2279d54cc2
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-11-14 15:16:46 +0700

    More layout updates

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -50,6 +50,6 @@ export default {
 
 <style scoped>
 .profile-page {
-  padding: 10px 10px 80px 10px;
+  padding: 10px 10px 0px 10px;
 }
 </style>
--- src/views/RewardsPage.vue
+++ src/views/RewardsPage.vue
@@ -61,6 +61,6 @@ export default {
 
 <style scoped>
 .rewards-page {
-  padding: 0px 10px 60px 10px;
+  padding: 0px 10px 0px 10px;
 }
 </style>
